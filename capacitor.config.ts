3314b6515fc6214aed554a52c128de26ab8fb225
Noriishere <noriyamashin@gmail.com>
2026-05-08 23:27:05 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'turn-based-rpg',
+  webDir: 'www'
+};
+
+export default config;
